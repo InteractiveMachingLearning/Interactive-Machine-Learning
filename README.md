ca8664a641b335f3ff8364f4665585165cce0cca
Jingxing Yang <jingxiny@usc.edu>
2018-03-24 20:00:02 -0700

    validate

--- README.md
+++
@@ -1,4 +0,0 @@
-# Data Collection Interface for Interactive Machine Learning
-
-This interface is deployed on http://ec2-54-167-221-37.compute-1.amazonaws.com:3000/ .
-
---
+++ validate.js
@@ -0,0 +1,109 @@
+
+var cnl = require('./checkNaturalLanguage.js');
+
+function validateRecords(serviceCodes)
+{
+	const serviceCodeLength = 3;
+	var rewards = [];
+	for(var i = 0; i < serviceCodes.length; i++)
+	{
+		rewards[i] = 0;
+		var len = serviceCodes[i].length;
+		if(len==serviceCodeLength&&(serviceCodes[i][len-1]=='A'||serviceCodes[i][len-1]=='B'))
+		{
+			rewards[i]=validate(serviceCode[i]);
+		}
+	}
+	return rewards;
+}
+
+function validate(serviceCode)
+{
+	const fs = require('fs');
+	var len = serviceCode.length;
+	var isUserA = serviceCode[len-1]=='A';
+	var reward = 0;
+	var recordName = serviceCode.substring(0,len-1) + '.txt';
+	const dir = "records/";
+
+	//reward calculation factor
+	////////////////////////////////////////
+	var rewardBase = 0.05; //reward of completing <= $lowerBound 
+	var lowerBound = 5;	//minimum number of questions to gain reward
+	var rewardPerExtraRound = 0.04; //reward per extra question above $lowerBound 
+	var GQPenalty = -0.05; //garbage quesiton penalty
+	var invalidLanguagePenalty = -0.05;
+	///////////////////////////////////////
+
+
+	var fileContents;
+	try
+	{
+		fileContents = fs.readFileSync(dir+recordName).toString();
+		var contents = JSON.parse(fileContents);
+		var records = contents.records;
+		//console.log(records);
+		if(isUserA)
+		{
+			//TODO
+			reward = rewardBase;
+			var num = 0;
+			for(var i =0; i < records.length; i++)
+			{
+				if(i%2==0)
+				{
+					var sentence = records[i][1][0];
+					isSentenceValid(test).then((isValid)=>{
+						if(isValid)
+						{
+							console.log("valid");
+						}
+						if(!isValid)
+						{
+							reward+=invalidLanguagePenalty;
+						}
+						else if(num > lowerBound)
+						{
+							reward += rewardPerExtraRound;
+						}
+					});
+				}
+			}
+		}
+		else
+		{
+			reward = rewardBase;
+			var num = 0;
+			for(var i = 0; i < records.length;i++)
+			{
+				if(i%2==1)
+				{
+					num++;
+					if(records[i][1][1]=="Garbage")
+					{
+						//console.log("Garbage Detected")
+						reward += GQPenalty;
+					}
+					else if(num > lowerBound)
+					{
+						reward += rewardPerExtraRound;
+					}
+				}
+			}
+		}
+		
+	}catch(err)
+	{
+		if(err.code === 'ENOENT')
+		{
+			console.log(dir+recordName+" not found!");
+		}
+	}
+
+	return Math.max(reward,0);
+
+}
+
+var serviceCode = ["11A"];
+var rewards = validateRecords(serviceCode);
+console.log(rewards);
