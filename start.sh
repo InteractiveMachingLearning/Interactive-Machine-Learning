14bb43df45b5645ba8953f7b80401fcd5e41a698
Jingxing Yang <jingxiny@usc.edu>
2018-04-17 17:24:28 -0700

    update start script

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ gcloud auth activate-service-account --key-file Chatbot-1261fee7b8c8.json
 #set environment variable
 export GOOGLE_APPLICATION_CREDENTIALS=Chatbot-1261fee7b8c8.json
 #start server, console output will be saved in log.txt
-node index.js |& tee -a log.txt
+node index.js |& tee log.txt
